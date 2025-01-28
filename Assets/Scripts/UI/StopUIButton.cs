1f83279d1da848a66f345be42ddcb4e82267a052
Thanos <loupas.thanos@gmail.com>
2025-01-28 19:07:40 +0200

    Timmer Added

--- Assets/Scripts/UI/StopUIButton.cs
+++ Assets/Scripts/UI/StopUIButton.cs
@@ -9,22 +9,69 @@ using UnityEngine.UI;
 
 public class StopUIButton : MonoBehaviour
 {
-    
+    [SerializeField] TMPro.TextMeshProUGUI text;
+    [SerializeField] private float timeLimit;
     [SerializeField] private Image firstCircle, secondCircle;
     [SerializeField] private Color32 activeColor;
     [SerializeField, Interface(typeof(IInteractableView))]
-    
+
     private UnityEngine.Object _interactableView;
     private int step = 0;
     private bool isPressEnabled;
+    private float remainingTime;
+    private bool isCountingDown;
+    
     private IInteractableView InteractableView { get; set; }
+    
+    [Space]
     public UnityEvent OnTwoTimesTapped;
-
+    public UnityEvent OnTimeOut;
+    
     private void OnEnable()
     {
         if(_interactableView)
             InteractableView.WhenStateChanged += UpdateVisualState;
     }
+
+    private void Start()
+    {
+        StartCountdown();
+    }
+    
+    private void StartCountdown()
+    {
+        remainingTime = timeLimit;
+        isCountingDown = true;
+        StartCoroutine(CountdownRoutine());
+    }
+
+    private void StopCountdown()
+    {
+        isCountingDown = false;
+        StopCoroutine(CountdownRoutine());
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        while (remainingTime > 0 && isCountingDown)
+        {
+            remainingTime -= Time.deltaTime;
+
+            // Update the text in the format "MM:SS"
+            TimeSpan timeSpan = TimeSpan.FromSeconds(remainingTime);
+            text.text = $"{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+
+            yield return null;
+        }
+
+        if (remainingTime <= 0)
+        {
+            text.text = "00:00";
+            text.color = Color.red;
+            OnTimeOut.Invoke(); // Trigger timeout event
+        }
+    }
+
 
     public void PressButton()
     {
@@ -71,5 +118,6 @@ public class StopUIButton : MonoBehaviour
     {
         if(_interactableView)
             InteractableView.WhenStateChanged -= UpdateVisualState;
+        StopCountdown();
     }
 }
